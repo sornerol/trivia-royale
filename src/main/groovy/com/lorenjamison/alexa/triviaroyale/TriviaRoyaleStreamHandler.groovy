0e8598fd3de8b006585af6050fe22e8cd4b2b642
Loren Jamison <loren.jamison@gmail.com>
2019-09-08 14:28:02 -0500

    Added skeleton classes for built-in handlers

--- src/main/groovy/com/lorenjamison/alexa/triviaroyale/TriviaRoyaleStreamHandler.groovy
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/TriviaRoyaleStreamHandler.groovy
@@ -1,2 +1,26 @@
 package com.lorenjamison.alexa.triviaroyale
 
+import com.amazon.ask.Skill
+import com.amazon.ask.Skills
+import com.amazon.ask.SkillStreamHandler
+
+import com.lorenjamison.alexa.triviaroyale.handlers.LaunchRequestHandler
+import com.lorenjamison.alexa.triviaroyale.handlers.HelpIntentHandler
+import com.lorenjamison.alexa.triviaroyale.handlers.CancelAndStopIntentHandler
+import com.lorenjamison.alexa.triviaroyale.handlers.SessionEndedRequestHandler
+
+class TriviaRoyaleStreamHandler extends SkillStreamHandler{
+    private static Skill getSkill() {
+        Skills.standard()
+                .addRequestHandlers(
+                        new LaunchRequestHandler(),
+                        new HelpIntentHandler(),
+                        new CancelAndStopIntentHandler(),
+                        new SessionEndedRequestHandler())
+                .build()
+    }
+
+    TriviaRoyaleStreamHandler() {
+        super(getSkill())
+    }
+}
