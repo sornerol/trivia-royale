170209ddbbb44ef6dfd8da2b85927a9bd845bc4e
Loren Jamison <loren.jamison@gmail.com>
2019-09-08 22:30:31 -0500

    Added utils package

---
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/utils/Data.groovy
@@ -0,0 +1,20 @@
+package com.lorenjamison.alexa.triviaroyale.utils
+
+class Data {
+    static final SKILL_TITLE = "Trivia Royale"
+
+    static final NEW_PLAYER_WELCOME_MESSAGE = "Welcome to Trivia Royale!"
+
+    static final EXISTING_PLAYER_WELCOME_MESSAGE = "Welcome back to Trivia Royale!"
+
+    static final HELP_MESSAGE = "Trivia Royale is the most intense quiz game ever. I'll pair you against 9 other " +
+            "players and ask you a series of questions. Try to answer each question correctly as fast as possible. " +
+            "Answer a question wrong or take too long to answer, and you will lose health. If your health reaches " +
+            "zero, you will be eliminated. If you survive to the final question, you'll receive leaderboard points " +
+            "based on your final place. Compete to take the top spot on the leaderboards every week. The more you " +
+            "play, the more points you'll earn."
+
+    static final CHOOSE_CATEGORY_MESSAGE = "To get started, tell me which category you want to play."
+
+    static final STARTING_NEW_GAME_MESSAGE = "Okay. Hold on while I find some opponents."
+}
