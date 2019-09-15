0e4dd3d4f079a80485cbabb5ba8409cb74e6d2f1
Loren Jamison <loren.jamison@gmail.com>
2019-09-15 13:45:31 -0500

    Revert "Added DataObject shell classes."
    This reverts commit 805650e5152fd25da26d841c65f66cda9097b01a.

--- src/main/groovy/com/lorenjamison/alexa/triviaroyale/dataobjects/DynamoDbTable.groovy
+++
@@ -1,9 +0,0 @@
-package com.lorenjamison.alexa.triviaroyale.dataobjects
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
-@Retention(RetentionPolicy.RUNTIME)
-@interface DynamoDbTable {
-    String value()
-}
--- src/main/groovy/com/lorenjamison/alexa/triviaroyale/dataobjects/GameDataObject.groovy
+++
@@ -1,5 +0,0 @@
-package com.lorenjamison.alexa.triviaroyale.dataobjects
-
-@DynamoDbTable("games")
-class GameDataObject {
-}
