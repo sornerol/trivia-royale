cd10071ffa6926e2a470df46a1ff9d718441bb8b
Loren Jamison <loren.jamison@gmail.com>
2019-10-02 20:25:35 -0500

    Added empty CreatePlayerIntentHandler.

---
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/handler/CreatePlayerIntentHandler.groovy
@@ -0,0 +1,17 @@
+package com.lorenjamison.alexa.triviaroyale.handler
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput
+import com.amazon.ask.dispatcher.request.handler.RequestHandler
+import com.amazon.ask.model.Response
+
+class CreatePlayerIntentHandler implements RequestHandler {
+    @Override
+    boolean canHandle(HandlerInput input) {
+        //TODO: Implement canHandle
+    }
+
+    @Override
+    Optional<Response> handle(HandlerInput input) {
+        //TODO: Implement handle
+    }
+}
