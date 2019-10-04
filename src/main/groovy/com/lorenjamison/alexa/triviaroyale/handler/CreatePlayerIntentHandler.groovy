3686367e89eef753106838e44b227693e77bbfa0
Loren Jamison <loren.jamison@gmail.com>
2019-10-03 21:56:35 -0500

    Began implementing CreatePlayerIntentHandler.

--- src/main/groovy/com/lorenjamison/alexa/triviaroyale/handler/CreatePlayerIntentHandler.groovy
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/handler/CreatePlayerIntentHandler.groovy
@@ -3,11 +3,16 @@ package com.lorenjamison.alexa.triviaroyale.handler
 import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
+import com.lorenjamison.alexa.triviaroyale.util.GameState
+import com.lorenjamison.alexa.triviaroyale.util.SessionAttributes
+
+import static com.amazon.ask.request.Predicates.intentName
+import static com.amazon.ask.request.Predicates.sessionAttribute
 
 class CreatePlayerIntentHandler implements RequestHandler {
     @Override
     boolean canHandle(HandlerInput input) {
-        //TODO: Implement canHandle
+        input.matches(intentName("NewPlayerIntent") & sessionAttribute(SessionAttributes.GAME_STATE, GameState.NEW_PLAYER_SETUP))
     }
 
     @Override
