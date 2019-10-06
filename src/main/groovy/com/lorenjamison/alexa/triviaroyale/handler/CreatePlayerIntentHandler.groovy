3faef0e2cf587c9b21839b39e0abc4320e59a993
Loren Jamison <loren.jamison@gmail.com>
2019-10-05 22:36:12 -0500

    Implemented CreatePlayerIntentHandler

--- src/main/groovy/com/lorenjamison/alexa/triviaroyale/handler/CreatePlayerIntentHandler.groovy
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/handler/CreatePlayerIntentHandler.groovy
@@ -2,15 +2,24 @@ package com.lorenjamison.alexa.triviaroyale.handler
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
+import com.amazon.ask.model.IntentRequest
 import com.amazon.ask.model.Response
+import com.amazon.ask.model.Slot
 import com.amazon.ask.response.ResponseBuilder
+import com.lorenjamison.alexa.triviaroyale.dataobject.Player
+import com.lorenjamison.alexa.triviaroyale.dataobject.base.PlayerBase
+import com.lorenjamison.alexa.triviaroyale.service.CategoryService
+import com.lorenjamison.alexa.triviaroyale.util.AlexaSdkHelper
+import com.lorenjamison.alexa.triviaroyale.util.Constants
 import com.lorenjamison.alexa.triviaroyale.util.GameState
+import com.lorenjamison.alexa.triviaroyale.util.Messages
 import com.lorenjamison.alexa.triviaroyale.util.SessionAttributes
 
 import static com.amazon.ask.request.Predicates.intentName
 import static com.amazon.ask.request.Predicates.sessionAttribute
 
 class CreatePlayerIntentHandler implements RequestHandler {
+    static final String NAME_SLOT_KEY = "name"
     @Override
     boolean canHandle(HandlerInput input) {
         input.matches(intentName("NewPlayerIntent") & sessionAttribute(SessionAttributes.GAME_STATE, GameState.NEW_PLAYER_SETUP))
@@ -20,8 +29,32 @@ class CreatePlayerIntentHandler implements RequestHandler {
     Optional<Response> handle(HandlerInput input) {
         ResponseBuilder response = input.getResponseBuilder()
         Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes()
-        //TODO: Implement handle
+        PlayerBase newPlayerBase = new PlayerBase()
+        newPlayerBase.with {
+            isHousePlayer = false
+            name = getPlayerName((IntentRequest) input.requestEnvelope.request)
+            alexaId = AlexaSdkHelper.getUserId(input)
+        }
+        Player newPlayer = new Player(newPlayerBase)
+
+        sessionAttributes.put(SessionAttributes.GAME_STATE, GameState.NEW_GAME)
+        String responseMessage = Messages.CHOOSE_CATEGORY_MESSAGE
+        String availableCategories = CategoryService.getCategoriesAvailableForPlayer(newPlayer)
+        responseMessage += availableCategories
+        String repromptMessage = availableCategories
+
+        response.with {
+            withSpeech(responseMessage)
+            withReprompt(repromptMessage)
+            withSimpleCard(Constants.SKILL_TITLE, responseMessage)
+            withShouldEndSession(false)
+        }
 
         response.build()
+    }
+
+    private String getPlayerName(IntentRequest intentRequest) {
+        Map <String, Slot> slots = intentRequest.intent.slots
+        slots[NAME_SLOT_KEY].value
     }
 }
