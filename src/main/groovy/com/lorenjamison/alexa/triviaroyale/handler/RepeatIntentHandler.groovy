95f0a96e4403581f87e5cf7b9d180e8e10201056
Loren Jamison <loren.jamison@gmail.com>
2019-10-05 22:11:28 -0500

    Added RepeatIntentHandler shell

---
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/handler/RepeatIntentHandler.groovy
@@ -0,0 +1,29 @@
+package com.lorenjamison.alexa.triviaroyale.handler
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput
+import com.amazon.ask.dispatcher.request.handler.RequestHandler
+import com.amazon.ask.model.Response
+import com.amazon.ask.response.ResponseBuilder
+import com.lorenjamison.alexa.triviaroyale.util.GameState
+import com.lorenjamison.alexa.triviaroyale.util.SessionAttributes
+
+import static com.amazon.ask.request.Predicates.intentName
+import static com.amazon.ask.request.Predicates.sessionAttribute
+
+class RepeatIntentHandler implements RequestHandler {
+    @Override
+    boolean canHandle(HandlerInput input) {
+        input.matches(intentName("AMAZON.RepeatIntent")
+                & (sessionAttribute(SessionAttributes.GAME_STATE, GameState.IN_GAME)
+                || sessionAttribute(SessionAttributes.GAME_STATE, GameState.NEW_GAME)))
+    }
+
+    @Override
+    Optional<Response> handle(HandlerInput input) {
+        ResponseBuilder response = input.getResponseBuilder()
+        Map<String, Object> sessionAttributes = input.getAttributesManager().getSessionAttributes()
+        //TODO: Implement handle
+
+        response.build()
+    }
+}
