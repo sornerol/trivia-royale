e316df6bdcc174ec885fa8736310a747d11da7f3
Loren Jamison <loren.jamison@gmail.com>
2019-12-03 06:48:04 -0600

    Added missing handlers to TriviaRoyaleStreamHandler

--- src/main/groovy/com/lorenjamison/alexa/triviaroyale/TriviaRoyaleStreamHandler.groovy
+++ src/main/groovy/com/lorenjamison/alexa/triviaroyale/TriviaRoyaleStreamHandler.groovy
@@ -4,22 +4,28 @@ import com.amazon.ask.Skill
 import com.amazon.ask.Skills
 import com.amazon.ask.SkillStreamHandler
 import com.amazon.ask.dispatcher.request.handler.impl.ConnectionsRequestHandler
+import com.lorenjamison.alexa.triviaroyale.handler.AnswerQuestionIntentHandler
 import com.lorenjamison.alexa.triviaroyale.handler.CreatePlayerIntentHandler
 import com.lorenjamison.alexa.triviaroyale.handler.HearRulesIntentHandler
 import com.lorenjamison.alexa.triviaroyale.handler.LaunchRequestHandler
 
 import com.lorenjamison.alexa.triviaroyale.handler.CancelAndStopIntentHandler
+import com.lorenjamison.alexa.triviaroyale.handler.NewGameIntentHandler
+import com.lorenjamison.alexa.triviaroyale.handler.RepeatIntentHandler
 import com.lorenjamison.alexa.triviaroyale.handler.SessionEndedRequestHandler
 
 class TriviaRoyaleStreamHandler extends SkillStreamHandler {
     private static Skill getSkill() {
         Skills.standard()
                 .addRequestHandlers(
-                        new LaunchRequestHandler(),
-                        new HearRulesIntentHandler(),
+                        new AnswerQuestionIntentHandler(),
                         new CancelAndStopIntentHandler(),
-                        new SessionEndedRequestHandler(),
-                        new CreatePlayerIntentHandler())
+                        new CreatePlayerIntentHandler(),
+                        new HearRulesIntentHandler(),
+                        new LaunchRequestHandler(),
+                        new NewGameIntentHandler(),
+                        new RepeatIntentHandler(),
+                        new SessionEndedRequestHandler())
                 .build()
     }
 
