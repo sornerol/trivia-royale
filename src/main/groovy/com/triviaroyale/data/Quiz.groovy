83ac3de2391c2deb8518e4a78c3a472fa08a62e5
Loren Jamison <loren.jamison@gmail.com>
2020-01-12 21:05:40 -0600

    Applied DynamoDB annotations to Quiz.groovy

--- src/main/groovy/com/triviaroyale/data/Quiz.groovy
+++ src/main/groovy/com/triviaroyale/data/Quiz.groovy
@@ -1,9 +1,16 @@
 package com.triviaroyale.data
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable
 
 @DynamoDBTable(tableName = 'TriviaRoyale')
 class Quiz {
-    long id
+    @DynamoDBHashKey
+    String category
+
+    @DynamoDBRangeKey
+    String uniqueId
+    
     List<Long> questionList
 }
