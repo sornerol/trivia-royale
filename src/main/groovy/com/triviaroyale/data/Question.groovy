5e293876855275eaf3496a9349ea381d9c26d0a8
Loren Jamison <loren.jamison@gmail.com>
2020-01-21 06:31:01 -0600

    Implemented Question.fromJson()

--- src/main/groovy/com/triviaroyale/data/Question.groovy
+++ src/main/groovy/com/triviaroyale/data/Question.groovy
@@ -1,13 +1,30 @@
 package com.triviaroyale.data
 
+import groovy.json.JsonSlurper
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 
 @CompileStatic
+@Slf4j
 class Question {
 
-    long id
     String questionText
     String correctAnswer
     List<String> otherAnswers
 
+    static Question fromJson(String json) {
+        JsonSlurper slurper = new JsonSlurper()
+        Question question
+        try {
+            question = slurper.parseText(json) as Question
+        } catch (IllegalArgumentException e) {
+            log.error('Could not create Question from JSON: ' + json)
+            throw e
+        }
+        question
+    }
+
+    String toJson() {
+        //TODO
+    }
 }
