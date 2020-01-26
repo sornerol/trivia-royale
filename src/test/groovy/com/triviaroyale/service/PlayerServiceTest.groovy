859e7e79ac4c6ba43a6b63c2c55a42a0eb57900e
Loren Jamison <loren.jamison@gmail.com>
2020-01-26 15:12:24 -0600

    Created empty Specification for PlayerServiceTest.

---
+++ src/test/groovy/com/triviaroyale/service/PlayerServiceTest.groovy
@@ -0,0 +1,11 @@
+package com.triviaroyale.service
+
+import spock.lang.Specification
+
+class PlayerServiceTest extends Specification {
+    def "LoadPlayer"() {
+    }
+
+    def "SavePlayer"() {
+    }
+}
