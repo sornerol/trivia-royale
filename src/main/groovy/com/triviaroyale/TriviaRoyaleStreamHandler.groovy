a9e2bad46d72193ae79d7ce359f63ad462e3d1ae
Loren Jamison <loren.jamison@gmail.com>
2020-02-11 21:40:41 -0600

    Formatting changes and imports optimized.

--- src/main/groovy/com/triviaroyale/TriviaRoyaleStreamHandler.groovy
+++ src/main/groovy/com/triviaroyale/TriviaRoyaleStreamHandler.groovy
@@ -1,16 +1,9 @@
 package com.triviaroyale
 
 import com.amazon.ask.Skill
-import com.amazon.ask.Skills
 import com.amazon.ask.SkillStreamHandler
-import com.triviaroyale.handler.NewGameIntentHandler
-import com.triviaroyale.handler.AnswerQuestionIntentHandler
-import com.triviaroyale.handler.CancelAndStopIntentHandler
-import com.triviaroyale.handler.CreatePlayerIntentHandler
-import com.triviaroyale.handler.HearRulesIntentHandler
-import com.triviaroyale.handler.LaunchRequestHandler
-import com.triviaroyale.handler.RepeatIntentHandler
-import com.triviaroyale.handler.SessionEndedRequestHandler
+import com.amazon.ask.Skills
+import com.triviaroyale.handler.*
 import groovy.transform.CompileStatic
 
 @CompileStatic
