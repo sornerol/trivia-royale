ff1f165d6b5245adec097734f2bdff2daa5b53c3
Loren Jamison <loren.jamison@gmail.com>
2020-05-10 23:26:18 -0500

    Rewrote CreatePlayerIntentHandler to use PlayerService.

--- src/main/groovy/com/triviaroyale/handler/CreatePlayerIntentHandler.groovy
+++ src/main/groovy/com/triviaroyale/handler/CreatePlayerIntentHandler.groovy
@@ -1,43 +1,51 @@
 package com.triviaroyale.handler
 
+import static com.amazon.ask.request.Predicates.intentName
+import static com.amazon.ask.request.Predicates.sessionAttribute
+
 import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
 import com.amazon.ask.response.ResponseBuilder
-
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.triviaroyale.data.Player
+import com.triviaroyale.service.PlayerService
+import com.triviaroyale.util.*
+import groovy.transform.CompileStatic
 
-import com.triviaroyale.util.AlexaSdkHelper
-import com.triviaroyale.util.AppState
-import com.triviaroyale.util.Messages
-import com.triviaroyale.util.SessionAttributes
-
-import static com.amazon.ask.request.Predicates.intentName
-import static com.amazon.ask.request.Predicates.sessionAttribute
-
+@CompileStatic
 class CreatePlayerIntentHandler implements RequestHandler {
+
     @Override
     boolean canHandle(HandlerInput input) {
-        input.matches(intentName('NewPlayerIntent') & sessionAttribute(SessionAttributes.GAME_STATE, AppState.NEW_PLAYER_SETUP))
+        input.matches(intentName('NewPlayerIntent')
+                & sessionAttribute(SessionAttributes.GAME_STATE, AppState.NEW_PLAYER_SETUP))
     }
 
     @Override
     Optional<Response> handle(HandlerInput input) {
-//        Map<String, Object> sessionAttributes = input.attributesManager.sessionAttributes
-//        PlayerBase newPlayerBase = new PlayerBase()
-//        newPlayerBase.with {
-//            name = AlexaSdkHelper.getSlotValue(input, AlexaSdkHelper.NAME_SLOT_KEY)
-//            alexaId = AlexaSdkHelper.getUserId(input)
-//        }
-//        Player newPlayer = Player.createNewPlayer(newPlayerBase)
-//
-//        sessionAttributes.put(SessionAttributes.GAME_STATE, AppState.NEW_GAME)
-//        sessionAttributes.put(SessionAttributes.PLAYER_ID, newPlayer.alexaId)
-//        String responseMessage = 'Thank you. ' + Messages.ASK_TO_START_NEW_GAME
-//        String repromptMessage = Messages.ASK_TO_START_NEW_GAME
-//
-//        sessionAttributes.put(SessionAttributes.LAST_RESPONSE, responseMessage)
-//        ResponseBuilder response = AlexaSdkHelper.responseWithSimpleCard(input, responseMessage, repromptMessage)
-//        response.build()
+        Map<String, Object> sessionAttributes = input.attributesManager.sessionAttributes
+        Player player = new Player()
+
+        player.with {
+            name = AlexaSdkHelper.getSlotValue(input, AlexaSdkHelper.NAME_SLOT_KEY)
+            alexaId = AlexaSdkHelper.getUserId(input)
+            quizCompletion = [:]
+        }
+
+        AmazonDynamoDB dynamoDB = AmazonAWSResourceHelper.openDynamoDBClient()
+
+        PlayerService playerService = new PlayerService(dynamoDB)
+        playerService.savePlayer(player)
+
+        sessionAttributes.put(SessionAttributes.GAME_STATE, AppState.NEW_GAME)
+
+        String responseMessage = 'Thank you. ' + Messages.ASK_TO_START_NEW_GAME
+        String repromptMessage = Messages.ASK_TO_START_NEW_GAME
+
+        sessionAttributes.put(SessionAttributes.LAST_RESPONSE, responseMessage)
+        ResponseBuilder response = AlexaSdkHelper.responseWithSimpleCard(input, responseMessage, repromptMessage)
+        response.build()
     }
+
 }
