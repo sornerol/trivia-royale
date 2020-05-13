1b7c8627fbb0959ba57b51ce6f87aa001561c1b1
Loren Jamison <loren.jamison@gmail.com>
2020-05-12 20:34:11 -0500

    Added empty methods to QuizService. [ci skip]

--- src/main/groovy/com/triviaroyale/service/QuizService.groovy
+++ src/main/groovy/com/triviaroyale/service/QuizService.groovy
@@ -1,6 +1,7 @@
 package com.triviaroyale.service
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.triviaroyale.data.GameState
 import com.triviaroyale.data.Quiz
 import com.triviaroyale.data.util.DynamoDBConstants
 import groovy.transform.CompileStatic
@@ -10,6 +11,18 @@ class QuizService extends DynamoDBAccess {
 
     QuizService(AmazonDynamoDB dynamoDB) {
         super(dynamoDB)
+    }
+
+    Quiz loadNextAvailableQuizForPlayer() {
+
+    }
+
+    Quiz generateNewQuiz() {
+
+    }
+
+    void addPerformanceToPool(GameState completedGame) {
+
     }
 
 }
