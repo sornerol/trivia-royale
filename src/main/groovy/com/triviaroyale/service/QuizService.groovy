9f988176e991209331235946583b4ebd4dd901f9
Loren Jamison <loren.jamison@gmail.com>
2020-05-23 23:32:46 -0500

    Added static method to get random opponents from the player pool for a quiz.[ci skip]

--- src/main/groovy/com/triviaroyale/service/QuizService.groovy
+++ src/main/groovy/com/triviaroyale/service/QuizService.groovy
@@ -3,7 +3,7 @@ package com.triviaroyale.service
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.triviaroyale.data.GameState
 import com.triviaroyale.data.Quiz
-import com.triviaroyale.data.util.DynamoDBConstants
+import com.triviaroyale.util.Constants
 import groovy.transform.CompileStatic
 
 @CompileStatic
@@ -13,11 +13,27 @@ class QuizService extends DynamoDBAccess {
         super(dynamoDB)
     }
 
-    Quiz loadNextAvailableQuizForPlayer() {
+    static String getQuizIdAsString(Quiz quiz) {
+        String quizId = quiz.category + '|' + quiz.uniqueId
+        quizId
+    }
+
+    static List<Tuple2<String, List<Boolean>>> getRandomPlayersForQuiz(Quiz quiz, int numberOfPlayers) {
+        List<Tuple2<String, List<Boolean>>> playerPool = quiz.playerPool
+        List<Tuple2<String, List<Boolean>>> selectedPlayers = []
+        Collections.shuffle(playerPool)
+        for (int i = 0; i < numberOfPlayers; i++) {
+            selectedPlayers.add(playerPool.pop())
+        }
+
+        selectedPlayers
+    }
+
+    Quiz loadNextAvailableQuizForPlayer(String playerId, String category = Constants.GENERAL_CATEGORY) {
 
     }
 
-    Quiz generateNewQuiz() {
+    Quiz generateNewQuiz(List<String> questions, String playerId, String category = Constants.GENERAL_CATEGORY) {
 
     }
 
