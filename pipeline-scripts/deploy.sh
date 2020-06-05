754002c3a877505224183fbbb2500b534a29d598
Loren Jamison <loren.jamison@gmail.com>
2020-06-04 19:50:03 -0500

    Fixed pipeline issues.

--- pipeline-scripts/deploy.sh
+++ pipeline-scripts/deploy.sh
@@ -7,4 +7,4 @@ aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME \
   --s3-bucket $S3_DEPLOY_BUCKET \
   --s3-key $PACKAGE_NAME
 
-aws s3 mv s3://$S3_DEPLOY_BUCKET/$PACKAGE_NAME s3://$S3_DEPLOY_BUCKET/deployed/$deployTime_$PACKAGE_NAME
+aws s3 mv s3://$S3_DEPLOY_BUCKET/$PACKAGE_NAME s3://$S3_DEPLOY_BUCKET/deployed/"$deployTime"-$PACKAGE_NAME
