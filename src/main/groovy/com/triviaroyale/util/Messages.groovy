10376c0b32030f92931f409a023c160c72119ddd
Loren Jamison <loren.jamison@gmail.com>
2020-06-08 22:40:41 -0500

    Fixed messages formatting.

--- src/main/groovy/com/triviaroyale/util/Messages.groovy
+++ src/main/groovy/com/triviaroyale/util/Messages.groovy
@@ -10,10 +10,10 @@ class Messages {
     public static final String WELCOME_EXISTING_PLAYER = 'Welcome back to Trivia Royale!'
 
     public static final String RULES =
-            """Test your brain power against other players in a battle royale style trivia game. The rules are simple. 
-Compete against ${Constants.NUMBER_OF_PLAYERS - 1} other players in a multiple choice trivia quiz. Correct answers will
-allow you to gain health points, while wrong answers will cause you to lose health.If your health drops to zero, you
-will be eliminated. Can you survive?"""
+            "Test your brain power against other players in a battle royale style trivia game. The rules are simple: \
+Compete against ${Constants.NUMBER_OF_PLAYERS - 1} other players in a multiple choice trivia quiz. Correct answers will \
+gain you health points, while wrong answers will make you lose health. If your health drops to zero, you \
+will be eliminated. Can you survive?"
 
     public static final String ASK_FOR_NAME = 'To get started, tell me your name.'
 
