af9d50188f1d66e6ae9a40064ea4b8fa9fc4d77e
Loren Jamison <loren.jamison@gmail.com>
2020-06-15 22:23:07 -0500

    [ci skip] Modified RepeatIntentHandler.canHandle() to always handle RepeatIntent.

--- src/main/groovy/com/triviaroyale/handler/RepeatIntentHandler.groovy
+++ src/main/groovy/com/triviaroyale/handler/RepeatIntentHandler.groovy
@@ -1,14 +1,12 @@
 package com.triviaroyale.handler
 
 import static com.amazon.ask.request.Predicates.intentName
-import static com.amazon.ask.request.Predicates.sessionAttribute
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
 import com.amazon.ask.response.ResponseBuilder
 import com.triviaroyale.util.AlexaSdkHelper
-import com.triviaroyale.util.AppState
 import com.triviaroyale.util.Constants
 import com.triviaroyale.util.SessionAttributes
 import groovy.transform.CompileStatic
@@ -20,9 +18,7 @@ class RepeatIntentHandler implements RequestHandler {
 
     @Override
     boolean canHandle(HandlerInput input) {
-        input.matches(intentName('AMAZON.RepeatIntent')) && (
-                sessionAttribute(SessionAttributes.APP_STATE, AppState.IN_GAME.toString()) ||
-                        sessionAttribute(SessionAttributes.APP_STATE, AppState.NEW_GAME.toString()))
+        input.matches(intentName('AMAZON.RepeatIntent'))
     }
 
     @Override
