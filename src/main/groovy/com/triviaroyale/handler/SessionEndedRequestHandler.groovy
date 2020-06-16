2d713de7a4a8913f423e59e7152aee353b9325d5
Loren Jamison <loren.jamison@gmail.com>
2020-06-15 23:27:56 -0500

    Implemented SessionEndedRequestHandler.handle() to save game state when in the middle of a game [ci skip].

--- src/main/groovy/com/triviaroyale/handler/SessionEndedRequestHandler.groovy
+++ src/main/groovy/com/triviaroyale/handler/SessionEndedRequestHandler.groovy
@@ -6,7 +6,13 @@ import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
 import com.amazon.ask.model.SessionEndedRequest
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.triviaroyale.data.GameState
+import com.triviaroyale.service.GameStateService
+import com.triviaroyale.util.AppState
 import com.triviaroyale.util.Constants
+import com.triviaroyale.util.SessionAttributes
 import groovy.transform.CompileStatic
 import groovy.util.logging.Log
 
@@ -23,7 +29,13 @@ class SessionEndedRequestHandler implements RequestHandler {
     Optional<Response> handle(HandlerInput input) {
         log.level = Constants.LOG_LEVEL
         log.entering(this.class.name, Constants.HANDLE_METHOD)
-
+        Map<String, Object> sessionAttributes = input.attributesManager.sessionAttributes
+        if (sessionAttributes[SessionAttributes.APP_STATE] == AppState.IN_GAME.toString()) {
+            GameState gameState = GameStateService.getSessionFromAlexaSessionAttributes(sessionAttributes)
+            AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient()
+            GameStateService gameStateService = new GameStateService(dynamoDB)
+            gameStateService.saveGameState(gameState)
+        }
         log.exiting(this.class.name, Constants.HANDLE_METHOD)
 
         null
