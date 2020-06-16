9eca036fecb774eadec7a9a6812e87694647038c
Loren Jamison <loren.jamison@gmail.com>
2020-06-15 23:21:47 -0500

    [ci skip] Added handlers for starting over and resuming games.

---
+++ src/main/groovy/com/triviaroyale/handler/StartOverIntentHandler.groovy
@@ -0,0 +1,23 @@
+package com.triviaroyale.handler
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput
+import com.amazon.ask.dispatcher.request.handler.RequestHandler
+import com.amazon.ask.model.Response
+import groovy.transform.CompileStatic
+import groovy.util.logging.Log
+
+@CompileStatic
+@Log
+class StartOverIntentHandler implements RequestHandler {
+
+    @Override
+    boolean canHandle(HandlerInput input) {
+
+    }
+
+    @Override
+    Optional<Response> handle (HandlerInput input) {
+
+    }
+
+}
