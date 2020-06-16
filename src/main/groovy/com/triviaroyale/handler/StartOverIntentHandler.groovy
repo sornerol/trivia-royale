56f0f4e5747936257f92b1d093d6926797cf7cb3
Loren Jamison <loren.jamison@gmail.com>
2020-06-16 13:04:58 -0500

    Implemented StartOverIntentHandler. [ci skip]

--- src/main/groovy/com/triviaroyale/handler/StartOverIntentHandler.groovy
+++ src/main/groovy/com/triviaroyale/handler/StartOverIntentHandler.groovy
@@ -1,8 +1,13 @@
 package com.triviaroyale.handler
 
+import static com.amazon.ask.request.Predicates.intentName
+import static com.amazon.ask.request.Predicates.sessionAttribute
+
 import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
+import com.amazon.ask.response.ResponseBuilder
+import com.triviaroyale.util.*
 import groovy.transform.CompileStatic
 import groovy.util.logging.Log
 
@@ -12,12 +17,25 @@ class StartOverIntentHandler implements RequestHandler {
 
     @Override
     boolean canHandle(HandlerInput input) {
-
+        input.matches(intentName('AMAZON.StartOverIntent') &
+                sessionAttribute(SessionAttributes.APP_STATE, AppState.IN_GAME.toString()))
     }
 
     @Override
-    Optional<Response> handle (HandlerInput input) {
+    Optional<Response> handle(HandlerInput input) {
+        log.level = Constants.LOG_LEVEL
+        log.entering(this.class.name, Constants.HANDLE_METHOD)
+        Map<String, Object> sessionAttributes = input.attributesManager.sessionAttributes
+        sessionAttributes[SessionAttributes.APP_STATE] = AppState.START_OVER_REQUEST
 
+        sessionAttributes[SessionAttributes.LAST_RESPONSE] = Messages.CONFIRM_START_OVER
+
+        input.attributesManager.sessionAttributes = sessionAttributes
+        ResponseBuilder responseBuilder = AlexaSdkHelper.responseWithSimpleCard(input,
+                Messages.CONFIRM_START_OVER,
+                Messages.CONFIRM_START_OVER)
+        log.exiting(this.class.name, Constants.HANDLE_METHOD)
+        responseBuilder.build()
     }
 
 }
