1f5a521eba6546a7ee426d47247c220168cbe9f0
Loren Jamison <loren.jamison@gmail.com>
2020-07-16 21:09:55 -0500

    Save current game state in CancelAndStopIntentHandler.handle()

--- src/main/groovy/com/triviaroyale/handler/CancelAndStopIntentHandler.groovy
+++ src/main/groovy/com/triviaroyale/handler/CancelAndStopIntentHandler.groovy
@@ -7,6 +7,10 @@ import com.amazon.ask.dispatcher.request.handler.HandlerInput
 import com.amazon.ask.dispatcher.request.handler.RequestHandler
 import com.amazon.ask.model.Response
 import com.amazon.ask.response.ResponseBuilder
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import com.triviaroyale.data.GameState
+import com.triviaroyale.service.GameStateService
 import com.triviaroyale.util.AppState
 import com.triviaroyale.util.Constants
 import com.triviaroyale.util.Messages
@@ -29,7 +33,14 @@ class CancelAndStopIntentHandler implements RequestHandler {
     @Override
     Optional<Response> handle(HandlerInput input) {
         log.fine(Constants.ENTERING_LOG_MESSAGE)
-
+        
+        Map<String, Object> sessionAttributes = input.attributesManager.sessionAttributes
+        if (sessionAttributes[SessionAttributes.APP_STATE] == AppState.IN_GAME.toString()) {
+            GameState gameState = GameStateService.getSessionFromAlexaSessionAttributes(sessionAttributes)
+            AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient()
+            GameStateService gameStateService = new GameStateService(dynamoDB)
+            gameStateService.saveGameState(gameState)
+        }
         ResponseBuilder responseBuilder = input.responseBuilder
                 .withSpeech(Messages.EXIT_SKILL)
                 .withSimpleCard(Constants.SKILL_TITLE, Messages.EXIT_SKILL)
